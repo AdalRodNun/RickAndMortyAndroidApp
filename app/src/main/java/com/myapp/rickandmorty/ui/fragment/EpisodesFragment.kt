67e7b0fcf06cdd9db0a94dfe0a5feb6d5b4a416c
AdalRodNun <adalberto.rn@outlook.com>
2024-03-15 16:29:04 -0600

    refactor(App): Changed navigation bar menus and icons.

---
+++ app/src/main/java/com/myapp/rickandmorty/ui/fragment/EpisodesFragment.kt
@@ -0,0 +1,30 @@
+package com.myapp.rickandmorty.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myapp.rickandmorty.databinding.FragmentEpisodesBinding
+
+class EpisodesFragment : Fragment() {
+
+    private lateinit var binding: FragmentEpisodesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEpisodesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
---
+++ app/src/main/java/com/myapp/rickandmorty/ui/fragment/LocationsFragment.kt
@@ -0,0 +1,30 @@
+package com.myapp.rickandmorty.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myapp.rickandmorty.databinding.FragmentLocationsBinding
+
+class LocationsFragment : Fragment() {
+
+    private lateinit var binding: FragmentLocationsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLocationsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
